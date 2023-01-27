988ee11ee8ff4698c178118dbdd5980f9e683ab2
Reinmic <maotoam@gloridauniversitaria.es>
2023-01-27 16:12:33 +0100

    script 1 marcel

---
+++ Script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+numLim=`cat disc.txt | wc -l`
+
+for i in `seq 1 $numLim`
+do
+    percent=`cat disc.txt | head -n$i | tail -1 | awk '{print $5}' | sed 's/%//'`
+    if [ $percent -ge 0 ] && [ $percent -le 1 ]
+    then
+        `cat disc.txt | head -n$i | tail -1 >> sol.txt`
+
+    fi
+done
+   
