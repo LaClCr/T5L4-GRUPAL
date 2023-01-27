4b92745b4fd2ba4ec9ade68746d3048b4ba28f37
Reinmic <maotoam@gloridauniversitaria.es>
2023-01-27 16:15:19 +0100

    actualizacion de mi script

--- Script1.sh
+++ Script1.sh
@@ -2,7 +2,7 @@
 
 numLim=`cat disc.txt | wc -l`
 
-for i in `seq 1 $numLim`
+for i in `seq 2 $numLim`
 do
     percent=`cat disc.txt | head -n$i | tail -1 | awk '{print $5}' | sed 's/%//'`
     if [ $percent -ge 0 ] && [ $percent -le 1 ]
